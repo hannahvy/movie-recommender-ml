f868218cc358378e1326247f1ee8ecf430658ed0
Hannah Nguyen <hnguy073@fiu.edu>
2020-10-16 22:12:17 -0400

    Added foundational code to Program.cs and MovieRatingData.cs to load in csv data

---
+++ MovieRecommender/MovieRatingData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.ML.Data;
+
+namespace MovieRecommender
+{
+    public class MovieRating // specifies input data class
+    {
+        [LoadColumn(0)] // specifies which columns (by index) should be loaded
+        public float userId; // feature
+        [LoadColumn(1)]
+        public float movieId; // feature
+        [LoadColumn(2)]
+        public float Label; // rating
+    }
+
+    public class MovieRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+}
--- MovieRecommender/Program.cs
+++ MovieRecommender/Program.cs
@@ -16,10 +16,25 @@ namespace MovieRecommender
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run(); // here by default
+            MLContext mlcontext = new MLContext(); // initializing creates new ML.NET enviornment
+            (IDataView trainingDataView, IDataView testDataView) = LoadData(mlcontext); // calls LoadData
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static (IDataView training, IDataView test) LoadData(MLContext mlContext)
+        {
+            // initialize data path variables
+            var trainingDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-train.csv");
+            var testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-test.csv");
+
+            // load data from csv files
+            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<MovieRating>(trainingDataPath, hasHeader: true, separatorChar: ',');
+            IDataView testDataView = mlContext.Data.LoadFromTextFile<MovieRating>(testDataPath, hasHeader: true, separatorChar: ',');
+
+            // return train and test data
+            return (trainingDataView, testDataView);
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) => // here by default
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
